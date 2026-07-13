17607e391bc0db3b2a983cbbeccfeb8bd9be60e3
Kaviya <kaviyamurugan29@gmail.com>
2026-07-13 20:03:31 +0530

    Configure frontend staging deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.127.100.12",
+  baseURL: "http://13.127.100.12:8001",
 
   headers: {
     "Content-Type": "application/json",
