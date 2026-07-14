b322dfa158cd928cac6dbb0a3b910d4c715510f4
Kaviya <kaviyamurugan29@gmail.com>
2026-07-14 12:11:20 +0530

    Update production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.127.100.12:8001",
+  baseURL: "http://13.127.100.12",
 
   headers: {
     "Content-Type": "application/json",
